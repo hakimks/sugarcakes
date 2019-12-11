dcde37fa86a925c08c2e1f843554686b83acad05
hakimks <hakimks@gmail.com>
2019-12-11 13:04:01 +0300

    Create and initilize the database helper class

--- app/src/main/java/com/kawesi/sugarcakes/MainActivity.java
+++ app/src/main/java/com/kawesi/sugarcakes/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.kawesi.sugarcakes.data.CakeDbHelper;
+
 public class MainActivity extends AppCompatActivity {
+    CakeDbHelper mCakeDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCakeDbHelper = new CakeDbHelper(this);
+
+
     }
 }
--- app/src/main/java/com/kawesi/sugarcakes/data/CakeDbHelper.java
+++ app/src/main/java/com/kawesi/sugarcakes/data/CakeDbHelper.java
@@ -1,4 +1,36 @@
 package com.kawesi.sugarcakes.data;
 
-public class CakeDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class CakeDbHelper extends SQLiteOpenHelper {
+    private static final String TAG = "CakeDbHelper";
+    //name of database
+    public static final String DATABASE_NAME = "cakes";
+    public static final int DATABASE_VERSION = 1;
+
+
+    public CakeDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CAKES_TABLE = "CREATE TABLE " + CakeContract.CakeEntry.CAKE_TABLE + "( " +
+                CakeContract.CakeEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                CakeContract.CakeEntry.CAKE_NAME + " TEXT NOT NULL, " +
+                CakeContract.CakeEntry.CAKE_INGREDIENTS + " TEXT NOT NULL, " +
+                CakeContract.CakeEntry.CAKE_PRICE + " INTEGER NOT NULL ); ";
+        db.execSQL(CREATE_CAKES_TABLE);
+        Log.i(TAG, "onCreate: Datbase created");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
