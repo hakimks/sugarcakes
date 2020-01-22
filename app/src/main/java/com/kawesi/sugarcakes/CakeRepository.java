8cc36e94c083a100d5ec3134ef61d3b3be6b3ac6
hakimks <hakimks@gmail.com>
2020-01-22 18:05:16 +0300

    Added a cake repository

---
+++ app/src/main/java/com/kawesi/sugarcakes/CakeRepository.java
@@ -0,0 +1,26 @@
+package com.kawesi.sugarcakes;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class CakeRepository {
+    private CakeDao mCakeDao;
+    private LiveData<List<Cake>> mAllCakes;
+
+    CakeRepository(Application application){
+        CakeRoomDatabase db = CakeRoomDatabase.getDatabase(application);
+        mCakeDao = db.mCakeDao();
+        mAllCakes = mCakeDao.getAllCakes();
+    }
+
+    LiveData<List<Cake>> getAllCakes(){
+        return mAllCakes;
+    }
+
+    void insertCake(Cake cake){
+        mCakeDao.insertCake(cake);
+    }
+}
