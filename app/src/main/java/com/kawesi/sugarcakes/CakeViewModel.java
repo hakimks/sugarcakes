8e733767c4ec28bf7ab24db2759af39221b9e707
hakimks <hakimks@gmail.com>
2020-01-22 18:09:35 +0300

    added a viewmodel class

---
+++ app/src/main/java/com/kawesi/sugarcakes/CakeViewModel.java
@@ -0,0 +1,29 @@
+package com.kawesi.sugarcakes;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class CakeViewModel extends AndroidViewModel {
+    private CakeRepository mCakeRepository;
+    private LiveData<List<Cake>> mAllCakes;
+
+    public CakeViewModel(@NonNull Application application) {
+        super(application);
+
+        mCakeRepository = new CakeRepository(application);
+        mAllCakes = mCakeRepository.getAllCakes();
+    }
+
+    LiveData<List<Cake>> getAllCakes(){
+        return mAllCakes;
+    }
+
+    public void insertCake(Cake cake){
+        mCakeRepository.insertCake(cake);
+    }
+}
