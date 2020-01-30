ed1c168c7d50300cfccf9cc1e7fb299ab1101903
hakimks <hakimks@gmail.com>
2020-01-30 12:14:21 +0300

    Added more dao interactions

--- app/src/main/java/com/kawesi/sugarcakes/CakeDao.java
+++ app/src/main/java/com/kawesi/sugarcakes/CakeDao.java
@@ -2,8 +2,10 @@ package com.kawesi.sugarcakes;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +14,13 @@ import java.util.List;
 public interface CakeDao {
 
     @Insert
-    void insertCake(Cake cake);
+    public void insertCake(Cake cake);
+
+    @Update
+    public void updateCake(Cake cake);
+
+    @Delete
+    public void deleteCake(Cake cake);
 
     @Query("DELETE FROM cake_table")
     void deleteAllCakes();
