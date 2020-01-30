eb5a47ed9f97c543b372eb1375ab24182411544d
hakimks <hakimks@gmail.com>
2020-01-30 17:34:57 +0300

    Added logic for all the four imageviews to be able to take a new photo using camera or select an existing photo from the gallery

--- app/src/main/java/com/kawesi/sugarcakes/CreateCakeActivity.java
+++ app/src/main/java/com/kawesi/sugarcakes/CreateCakeActivity.java
@@ -1,12 +1,16 @@
 package com.kawesi.sugarcakes;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.ClipData;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -26,6 +30,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.kawesi.sugarcakes.data.CakeCategory;
 
@@ -39,6 +44,10 @@ import java.util.List;
 public class CreateCakeActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY = "com.kawei.sugarcakes.REPLY";
     public static final String IMAGE_VIEW_NAME = "imageViewName";
+    private static final String[] PERMISSION_STORAGE = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    } ;
+    private static final int CODE_WRITE_STORAGE_PERMISSION = 300;
     private EditText cakeName, cakePrice, cakeIngredients;
     private ImageView cakeImageView, otherImage1View, otherImage2View, otherImage3View;
     private String name, price, ingredients;
@@ -48,16 +57,46 @@ public class CreateCakeActivity extends AppCompatActivity {
 
 
     private Spinner categorySpinner;
+    // we have four sets of images, that will mean four codes of gallery request and four of camera request
+    public static final int MAIN_IMG_CAMERA_REQUEST_CODE = 100;
+    public static final int MAIN_IMG_GALLERY_REQUEST_CODE = 200;
 
-    public static final int GALLERY_REQUEST_CODE = 200;
-    private static final int REQUEST_IMAGE_CAPTURE = 100;
+    public static final int OTHER_IMG_ONE_CAMERA_REQUEST_CODE = 101;
+    public static final int OTHER_IMG_ONE_GALLERY_REQUEST_CODE = 201;
+
+    public static final int OTHER_IMG_TWO_CAMERA_REQUEST_CODE = 102;
+    public static final int OTHER_IMG_TWO_GALLERY_REQUEST_CODE = 202;
+
+    public static final int OTHER_IMG_THREE_CAMERA_REQUEST_CODE = 103;
+    public static final int OTHER_IMG_THREE_GALLERY_REQUEST_CODE = 203;
+
+    // Path and uri for the four images
+    private String currentCameraPathMainImg;
+    private Uri mainImageGlobalUri;
+
+    private String currentCameraPathOtherImgOne;
+    private Uri otherImageOneGlobalUri;
+
+    private String currentCameraPathOtherImgTwo;
+    private Uri otherImageTwoGlobalUri;
+
+    private String currentCameraPathOtherImgThree;
+    private Uri otherImageThreeGlobalUri;
+
+
+
+    public static final int GALLERY_REQUEST_CODE = 2000;
+    private static final int REQUEST_IMAGE_CAPTURE = 1000;
     private String currentPhotoPath;
 
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_cake);
+
+        mContext = this;
 
         categorySpinner = (Spinner) findViewById(R.id.category_spinner);
         String[] items = CakeCategory.categoryMap.values().toArray(new String[]{});
@@ -66,23 +105,45 @@ public class CreateCakeActivity extends AppCompatActivity {
         categorySpinner.setAdapter(adapter);
 
         cakeImageView = (ImageView) findViewById(R.id.createcake_imageView);
+        cakeImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mainImage = "mainImage";
+                selectOrTakeImage(mContext, MAIN_IMG_GALLERY_REQUEST_CODE, MAIN_IMG_CAMERA_REQUEST_CODE, mainImage);
+
+            }
+        });
         otherImage1View = (ImageView) findViewById(R.id.otherPicture1_imageView);
+        otherImage1View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String otherImageOne = "otherImageOne";
+                selectOrTakeImage(mContext, OTHER_IMG_ONE_GALLERY_REQUEST_CODE, OTHER_IMG_ONE_CAMERA_REQUEST_CODE, otherImageOne);
+            }
+        });
         otherImage2View = (ImageView) findViewById(R.id.otherPicture2_imageView);
+        otherImage2View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String otherImageTwo = "otherImageTwo";
+                selectOrTakeImage(mContext, OTHER_IMG_TWO_GALLERY_REQUEST_CODE, OTHER_IMG_TWO_CAMERA_REQUEST_CODE, otherImageTwo);
+
+            }
+        });
         otherImage3View = (ImageView) findViewById(R.id.otherPicture3_imageView);
+        otherImage3View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String otherImageThree = "otherImageThree";
+                selectOrTakeImage(mContext, OTHER_IMG_THREE_GALLERY_REQUEST_CODE, OTHER_IMG_THREE_CAMERA_REQUEST_CODE, otherImageThree);
+            }
+        });
 
         cakeName = findViewById(R.id.cakeNameEditText);
         cakePrice = findViewById(R.id.price_editText);
         final Date currentTime = Calendar.getInstance().getTime();
         cakeIngredients = findViewById(R.id.ingredients_editText);
 
-
-        cakeImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showPictureDialog();    // passin the image view
-
-            }
-        });
 
         final Button saveButton = findViewById(R.id.save_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
@@ -121,94 +182,205 @@ public class CreateCakeActivity extends AppCompatActivity {
 
     }
 
-    private void showPictureDialog() {
-        AlertDialog.Builder pictureDialog =  new AlertDialog.Builder(this);
-        pictureDialog.setTitle("Select Action");
-        String[] picDialogItems = {"Select Photo From Gallery", "Capture Photo from Camera"};
-        pictureDialog.setItems(picDialogItems, new DialogInterface.OnClickListener() {
+    private void selectOrTakeImage(final Context context, final int galleryRequestCode, final int cameraRequestCode, final String imageNumber ){
+        final CharSequence[] options = {"Take Photo", "Choose From Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose/Take Cake Photo");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                switch (which){
-                    case 0:
-                        dispatchSelectPictureIntent();
-                        break;
-                    case 1:
-                        distaptchCameraIntent();
-                        break;
+                if (options[which].equals("Take Photo")){
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (cameraIntent.resolveActivity(getPackageManager()) != null){
+                        File imageFile = null;
+                        try {
+
+                            if (imageNumber.equals("mainImage")){
+                                imageFile = createImageFile("mainImage");
+
+                            }
+                            if (imageNumber.equals("otherImageOne")){
+                                imageFile = createImageFile("otherImageOne");
+
+                            }
+                            if (imageNumber.equals("otherImageTwo")){
+                                imageFile = createImageFile("otherImageTwo");
+
+                            }
+                            if (imageNumber.equals("otherImageThree")){
+                                imageFile = createImageFile("otherImageThree");
+
+                            }
+
+                        } catch (Exception ex){
+                            ex.printStackTrace();
+                        }
+
+                        if (imageFile != null){
+                            Uri photoUri = FileProvider.getUriForFile(context, "com.kawesi.sugarcakes.fileprovider", imageFile);
+
+                            if (imageNumber.equals("mainImage")){
+                                mainImageGlobalUri = photoUri;
+
+                            }else
+                            if (imageNumber.equals("otherImageOne")){
+                                otherImageOneGlobalUri = photoUri;
+
+                            } else
+                            if (imageNumber.equals("otherImageTwo")){
+                                otherImageTwoGlobalUri = photoUri;
+
+                            } else
+                            if (imageNumber.equals("otherImageThree")){
+                                otherImageThreeGlobalUri = photoUri;
+
+                            }
+
+                            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                            startActivityForResult(cameraIntent, cameraRequestCode);
+                        }
+                    }
+                }else
+                if (options[which].equals("Choose From Gallery")){
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(galleryIntent, galleryRequestCode);
+
+
+                } else
+                if (options[which].equals("Cancel")){
+                    dialog.dismiss();
                 }
             }
         });
-        pictureDialog.show();
+
+        builder.create().show();
     }
 
-    private void dispatchSelectPictureIntent() {
-        Intent selectOrTakePhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        selectOrTakePhotoIntent.setType("image/*");
-        String[] mimeTypes = {"image/jpeg", "image/jpg", "image/png"};
-        selectOrTakePhotoIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-        startActivityForResult(selectOrTakePhotoIntent, GALLERY_REQUEST_CODE);
-
-    }
-
-    private void distaptchCameraIntent() {
-        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if (takePhotoIntent.resolveActivity(getPackageManager()) != null){
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-            if (photoFile != null){
-                Uri photoUri = FileProvider.getUriForFile(this,
-                        "com.kawesi.sugarcakes.fileprovider", photoFile);
 
 
-                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
-            }
 
 
-        }
-    }
 
-    private File createImageFile() throws IOException {
+    private File createImageFile(String imageNo) throws IOException {
         String timeSTamp = new SimpleDateFormat("yyyyMMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeSTamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
+        // check for permission
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) == PackageManager.PERMISSION_GRANTED){
+            File storageDirExt = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            File image = File.createTempFile(
+                    imageFileName,
+                    ".jpg",
+                    storageDirExt
+            );
+            if (imageNo.equals("mainImage")){
+               currentCameraPathMainImg = image.getAbsolutePath();
 
-        return image;
+            }else
+            if (imageNo.equals("otherImageOne")){
+                currentCameraPathOtherImgOne = image.getAbsolutePath();
+
+            } else
+            if (imageNo.equals("otherImageTwo")){
+                currentCameraPathOtherImgTwo = image.getAbsolutePath();
+
+            } else
+            if (imageNo.equals("otherImageThree")){
+                currentCameraPathOtherImgThree = image.getAbsolutePath();
+            }
+
+            return  image;
+        } else {
+            requestPermissions();
+            return null;
+        }
+
+    }
+
+    private void requestPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Permission Needed");
+            builder.setMessage("This permission is needed to be able to save images to the Phone gallery");
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions(CreateCakeActivity.this,
+                            PERMISSION_STORAGE, CODE_WRITE_STORAGE_PERMISSION);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            builder.create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, PERMISSION_STORAGE, CODE_WRITE_STORAGE_PERMISSION);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE){
-            Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
-            cakeImageView.setImageBitmap(bitmap);
+        if (resultCode == RESULT_OK){
+            switch (requestCode){
+                case MAIN_IMG_CAMERA_REQUEST_CODE:
+                    cakeImageView.setImageURI(mainImageGlobalUri);
+                    galleryAddPic(currentCameraPathMainImg);
+                    break;
+                case MAIN_IMG_GALLERY_REQUEST_CODE:
+                    Uri selectedImg = data.getData();
+                    cakeImageView.setImageURI(selectedImg);
+                    break;
+                case OTHER_IMG_ONE_CAMERA_REQUEST_CODE:
+                    otherImage1View.setImageURI(otherImageOneGlobalUri);
+                    galleryAddPic(currentCameraPathOtherImgOne);
+                    break;
+                case OTHER_IMG_ONE_GALLERY_REQUEST_CODE:
+                    Uri selectedImg1 = data.getData();
+                    otherImage1View.setImageURI(selectedImg1);
+                    break;
+                case OTHER_IMG_TWO_CAMERA_REQUEST_CODE:
+                    otherImage2View.setImageURI(otherImageTwoGlobalUri);
+                    galleryAddPic(currentCameraPathOtherImgTwo);
+                    break;
+                case OTHER_IMG_TWO_GALLERY_REQUEST_CODE:
+                    Uri selectedImg2 = data.getData();
+                    otherImage2View.setImageURI(selectedImg2);
+                    break;
+                case OTHER_IMG_THREE_CAMERA_REQUEST_CODE:
+                    otherImage1View.setImageURI(otherImageThreeGlobalUri);
+                    galleryAddPic(currentCameraPathOtherImgThree);
+                    break;
+                case OTHER_IMG_THREE_GALLERY_REQUEST_CODE:
+                    Uri selectedImg3 = data.getData();
+                    otherImage3View.setImageURI(selectedImg3);
+                    break;
+            }
         }
 
-        if (requestCode == GALLERY_REQUEST_CODE){
-            if (data != null){
-                Uri contetUri = data.getData();
-                try{
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contetUri);
-                    cakeImageView.setImageBitmap(bitmap);
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == CODE_WRITE_STORAGE_PERMISSION){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission Denied, you wont be able to save pictures", Toast.LENGTH_SHORT).show();
             }
 
         }
     }
 
-
-
+    private void galleryAddPic(String currentPath){
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
 
 }
